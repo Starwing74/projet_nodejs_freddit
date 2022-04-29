c74185d73912a3778d3c6800fe22d071a1e5de2f
sxing26 <69822358+sxing26@users.noreply.github.com>
2022-04-29 09:10:29 +0200

    [NEW] method to establish connection with DB

--- app/index.js
+++ app/index.js
@@ -1,1 +1,25 @@
-const express = require('express');
+require('dotenv').config();
+const express = require("express");
+const path = require('path');
+const app = express();
+
+const mongoose = require('mongoose');
+
+const port = process.env.PORT;
+/**
+ * password: FJbwSvBOP2gcfI1c
+ */
+mongoose.connect(
+    process.env.DB_CONNECTION
+).then((res) => {
+    console.log('connected to database');
+}).then((res) => {
+    app.listen(port, () => {
+        console.log(`Listening on port ${port}`);
+    })
+}).catch((err) => {
+    console.log(err);
+})
+
+app.use(express.json());
+
