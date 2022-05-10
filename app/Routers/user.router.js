498443313456031fec0ea5fc382bb3da9c599d52
Starwing74 <84375935+Starwing74@users.noreply.github.com>
2022-05-10 08:32:32 +0200

    samuel 19

--- app/Routers/user.router.js
+++ app/Routers/user.router.js
@@ -1,30 +1,21 @@
 const express = require('express');
 const userController = require('../Controllers/user.controller');
-const sessiontesting = require("express-session");
 const getSession = require("../Middleware/session");
 const verifyToken = require("../Middleware/auth");
+const session = require("express-session");
+const path = require("path");
 
 const userRouter = express.Router();
 
 userRouter.get('/register', userController.pageInscription);
 userRouter.post('/post', userController.postUser);
-userRouter.get('/connexion', userController.pageConnexion);
-userRouter.get('/checkConnexion', getSession, userController.checkConnexion);
-userRouter.put('/update', getSession, verifyToken, userController.updateUser);
-userRouter.delete('/delete', getSession, verifyToken, userController.deleteUser);
-userRouter.put('/joinCommunity', getSession, verifyToken, userController.userJoinCommunity);
-userRouter.put('/leaveCommunity', getSession, verifyToken, userController.userLeaveCommunity);
-
-userRouter.use(sessiontesting({
-    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
-    saveUninitialized:true,
-    cookie: { maxAge: 60000  },
-    resave: false
-}));
-
-userRouter.get("/testing", function(req, res) {
-    sessiontesting.fullname = "testting testing";
-    console.log(sessiontesting)
-});
+userRouter.post('/connexion', userController.pageConnexion);
+userRouter.post('/checkConnexion', userController.checkConnexion);
+userRouter.get('/update', verifyToken,userController.updateUser);
+userRouter.delete('/delete', verifyToken, userController.deleteUser);
+userRouter.put('/joinCommunity', verifyToken, userController.userJoinCommunity);
+userRouter.put('/leaveCommunity', verifyToken, userController.userLeaveCommunity);
+userRouter.post('/deconnexion', userController.deconnexion);
+userRouter.post('/info', verifyToken, userController.infoUser);
 
 module.exports = userRouter;
